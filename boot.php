764b44968d9619cc1df378c3a6dd62073a426b3d
Alexander Walther <github.com@alexplus.de>
2020-04-06 10:48:47 +0200

    route category, location

--- boot.php
+++ boot.php
@@ -17,7 +17,7 @@ rex_yform_manager_dataset::setModelClass(
 /* YForm Rest API */
 $rex_event_date_route = new \rex_yform_rest_route(
     [
-        'path' => '/v0.1/event/date/',
+        'path' => '/v0.dev/event/date/',
         'auth' => '\rex_yform_rest_auth_token::checkToken',
         'type' => \event_date::class,
         'query' => \event_date::query(),
@@ -26,11 +26,31 @@ $rex_event_date_route = new \rex_yform_rest_route(
                 'rex_event_date' => [
                     'id',
                     'name',
+                    'description',
+                    'location',
+                    'image',
+                    'startDate',
+                    'doorTime',
+                    'endDate',
+                    'eventStatus',
+                    'offers_url',
+                    'offers_price',
+                    'offers_availability',
                     'url'
+                 ],
+                 'rex_event_category' => [
+                    'id',
+                    'name',
+                    'image'
                  ],
                  'rex_event_location' => [
                     'id',
-                    'name'
+                    'name',
+                    'street',
+                    'zip',
+                    'locality',
+                    'lat',
+                    'lng'
                  ]
             ]
         ],
@@ -38,6 +58,16 @@ $rex_event_date_route = new \rex_yform_rest_route(
             'fields' => [
                 'rex_event_date' => [
                     'name',
+                    'description',
+                    'location',
+                    'image',
+                    'startDate',
+                    'doorTime',
+                    'endDate',
+                    'eventStatus',
+                    'offers_url',
+                    'offers_price',
+                    'offers_availability',
                     'url'
                 ]
             ]
@@ -52,5 +82,86 @@ $rex_event_date_route = new \rex_yform_rest_route(
     ]
 );
 
-// Einbinden der Konfiguration
-\rex_yform_rest::addRoute($route);
+\rex_yform_rest::addRoute($rex_event_date_route);
+
+
+/* YForm Rest API */
+$rex_event_category_route = new \rex_yform_rest_route(
+    [
+        'path' => '/v0.dev/event/category/',
+        'auth' => '\rex_yform_rest_auth_token::checkToken',
+        'type' => \event_category::class,
+        'query' => \event_category::query(),
+        'get' => [
+            'fields' => [
+                 'rex_event_category' => [
+                    'id',
+                    'name',
+                    'image'
+                 ]
+            ]
+        ],
+        'post' => [
+            'fields' => [
+                'rex_event_category' => [
+                    'name',
+                    'image'
+                ]
+            ]
+        ],
+        'delete' => [
+            'fields' => [
+                'rex_event_category' => [
+                    'id'
+                ]
+            ]
+        ]
+    ]
+);
+
+\rex_yform_rest::addRoute($rex_event_category_route);
+
+/* YForm Rest API */
+$rex_event_location_route = new \rex_yform_rest_route(
+    [
+        'path' => '/v0.dev/event/location/',
+        'auth' => '\rex_yform_rest_auth_token::checkToken',
+        'type' => \event_location::class,
+        'query' => \event_location::query(),
+        'get' => [
+            'fields' => [
+                 'rex_event_location' => [
+                    'id',
+                    'name',
+                    'street',
+                    'zip',
+                    'locality',
+                    'lat',
+                    'lng'
+                 ]
+            ]
+        ],
+        'post' => [
+            'fields' => [
+                'rex_event_location' => [
+                    'name',
+                    'name',
+                    'street',
+                    'zip',
+                    'locality',
+                    'lat',
+                    'lng'
+                ]
+            ]
+        ],
+        'delete' => [
+            'fields' => [
+                'rex_event_location' => [
+                    'id'
+                ]
+            ]
+        ]
+    ]
+);
+
+\rex_yform_rest::addRoute($rex_event_location_route);
