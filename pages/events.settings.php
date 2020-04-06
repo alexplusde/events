86562015d313edd7a64f5ff14209f09ab3ab16fa
Alexander Walther <github.com@alexplus.de>
2020-04-06 17:54:33 +0200

    add currency config

--- pages/events.settings.php
+++ pages/events.settings.php
@@ -1,6 +1,13 @@
 <?php
 
 $addon = rex_addon::get('events');
+
+$form = rex_config_form::factory($addon->name);
+
+$field = $form->addInputField('text', 'currency', null, ["class" => "form-control"]);
+$field->setLabel('Währung');
+
+
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $addon->i18n('events_settings'), false);
