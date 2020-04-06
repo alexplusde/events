64df724e02e492365f89b1bb461dd9a132b32f21
alexplusde <github.com@alexplus.de>
2020-04-06 21:40:47 +0200

    remove plugin, add built-in support

--- plugins/lang/install.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Tablesets aktualisieren */
-
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_event_lang.tableset.json')));
-
-rex_yform_manager_table::deleteCache();
