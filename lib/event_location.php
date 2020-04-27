a62a8e9c5ebea5f51e56d743bcb071f0ac49f87e
191977mbp <mail@schuler-it-consulting.de>
2020-04-27 16:33:10 +0200

    event location get single fields added

--- lib/event_location.php
+++ lib/event_location.php
@@ -5,4 +5,22 @@ class event_location extends \rex_yform_manager_dataset
     {
         return $this->getValue('street') .", ". $this->getValue('zip') .", ".$this->getValue('locality').", ".$this->getValue('countrycode');
     }
+    public function getLocationStreet() :string
+    {
+        return $this->getValue('street');
+    }
+    
+    public function getLocationZip() :string
+    {
+        return $this->getValue('zip');
+    }
+    
+    public function getLocationLocality() :string
+    {
+        return $this->getValue('locality');
+    }
+    public function getLocationCountrycode() :string
+    {
+        return $this->getValue('countrycode');
+    }
 }
