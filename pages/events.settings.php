082570f9bb9597e293c0bdf07f757722f5a79971
alexplusde <github.com@alexplus.de>
2020-05-07 17:36:15 +0200

    Update events.settings.php

--- pages/events.settings.php
+++ pages/events.settings.php
@@ -7,6 +7,9 @@ $form = rex_config_form::factory($addon->name);
 $field = $form->addInputField('text', 'currency', null, ["class" => "form-control"]);
 $field->setLabel(rex_i18n::msg('events_currency'));
 
+$field = $form->addInputField('text', 'timezone_api_code', null, ["class" => "form-control"]);
+$field->setLabel(rex_i18n::msg('events_timezone'));
+
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $addon->i18n('events_settings'), false);
