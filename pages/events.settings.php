0ef3c2627be60b8a38da65e257aa1709aadb473c
alexplusde <github.com@alexplus.de>
2020-05-07 17:41:01 +0200

    label verbessern

--- pages/events.settings.php
+++ pages/events.settings.php
@@ -9,6 +9,7 @@ $field->setLabel(rex_i18n::msg('events_currency'));
 
 $field = $form->addInputField('text', 'timezone_api_code', null, ["class" => "form-control"]);
 $field->setLabel(rex_i18n::msg('events_timezone'));
+$field->setNotice(rex_i18n::msg('events_timezone_notice'). '<a href="https://developers.google.com/maps/documentation/timezone/intro?hl=de">https://developers.google.com/maps/documentation/timezone/intro?hl=de</a>');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
