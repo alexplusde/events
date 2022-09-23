275b995e5a32d24ca2c7405545c3fd2537ed61b4
Alexander Walther <aw@alexplus.de>
2022-09-23 20:09:02 +0200

    wording "Beauftragung"

--- pages/events.settings.php
+++ pages/events.settings.php
@@ -45,7 +45,7 @@ $fragment->setVar('body', $form->get(), false);
     <div class="col-lg-4">
         <?php
 
-$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=events"><img src="'.rex_url::addonAssets('events', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=events"><img src="'.rex_url::addonAssets('events', 'jetzt-beauftragen.svg').'" style="width: 100% max-width: 400px;"></a>';
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'info', false);
