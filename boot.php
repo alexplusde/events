22cce4a89d298dd6c7e2e69550af9d89fbe2faa1
Alexander Walther <aw@alexplus.de>
2023-01-05 15:51:17 +0100

    move editor settings, ep from msg to events

--- boot.php
+++ boot.php
@@ -1,5 +1,15 @@
 <?php
 
+if (rex::isBackend() && rex_be_controller::getCurrentPage() == "events/date") {
+    rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
+        $suchmuster = '###events-settings-editor###';
+        $ersetzen = rex_config::get("events", "editor");
+        $ep->setSubject(str_replace($suchmuster, $ersetzen, $ep->getSubject()));
+    });
+}
+
+rex_extension::register('REX_YFORM_SAVED', ['event_registration', 'ep_saved'], rex_extension::LATE);
+
 if (rex::isBackend() && rex_be_controller::getCurrentPage() == 'events/calendar') {
     rex_view::addJsFile($this->getAssetsUrl('fullcalendar/packages/core/main.js'));
     rex_view::addCssFile($this->getAssetsUrl('fullcalendar/packages/core/main.css'));
@@ -191,13 +201,6 @@ if (rex_plugin::get('yform', 'rest')->isAvailable() && !rex::isSafeMode()) {
 
     \rex_yform_rest::addRoute($rex_event_location_route);
 }
-
-rex_extension::register('REX_YFORM_SAVED', function (rex_extension_point $ep) {
-    // darf nur bei passender Tabelle passieren.
-//    $id = $ep->getParam('id');
-//    $dataset = event_date::get($ep->getParam('id'));
-//    rex_sql::factory()->setQuery("UPDATE rex_event_date SET uid = :uid WHERE id = :id", [":uid"=>$dataset->getUid(), ":id" => $id]);
-});
 
 rex_extension::register('YFORM_DATA_LIST', function ($ep) {
     if ($ep->getParam('table')->getTableName()=="rex_event_date") {
