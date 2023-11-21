b7e5777fc5d40a0671f581de83e7739f4d777f0d
Paul Götz <20090597+schorschy@users.noreply.github.com>
2023-11-21 11:23:31 +0100

    Update event_registration_person.php

--- lib/event_registration_person.php
+++ lib/event_registration_person.php
@@ -2,7 +2,7 @@
 
 class event_registration_person extends \rex_yform_manager_dataset
 {
-    public function getName($reverse = false)
+    public function getName($reverse = false): string
     {
         $name = [];
         if ($reverse) {
@@ -15,23 +15,23 @@ class event_registration_person extends \rex_yform_manager_dataset
         }
         return implode(" ", $name);
     }
-    public function getFirstName()
+    public function getFirstName(): string
     {
         return $this->getValue('firstname');
     }
-    public function getLastName()
+    public function getLastName(): string
     {
         return $this->getValue('lastname');
     }
-    public function getMail()
+    public function getMail(): string
     {
         return $this->getValue('email');
     }
-    public function getPhone()
+    public function getPhone(): string
     {
         return $this->getValue('phone');
     }
-    public function getBirthday()
+    public function getBirthday(): string
     {
         return $this->getValue('birthday');
     }
@@ -45,50 +45,50 @@ class event_registration_person extends \rex_yform_manager_dataset
         }
     }
     
-    public function getStatus()
+    public function getStatus(): int
     {
         return $this->getValue('status');
     }
 
-    public function getEventDateId()
+    public function getEventDateId(): int
     {
         return $this->getValue('event_date_id');
     }
-    public function getRegistrationId()
+    public function getRegistrationId(): int
     {
         return $this->getValue('registration_id');
     }
 
-    public function getRegistration()
+    public function getRegistration(): object
     {
         return $this->getRelatedDataset('registration_id');
     }
 
-    public function getCategoryId()
+    public function getCategoryId(): int
     {
         return $this->getRegistration()->getCategory()->getId();
     }
 
-    public function getHash()
+    public function getHash(): string
     {
         return $this->getValue('hash');
     }
     
-    public static function getByUuid($uuid = null)
+    public static function getByUuid($uuid = null): object
     {
         if (!$uuid) {
             return;
         }
         return self::query()->where('uuid', $uuid)->findOne();
     }
-    public static function getByHash($hash = null)
+    public static function getByHash($hash = null): object
     {
         if (!$hash) {
             return;
         }
         return self::query()->where('hash', $hash)->findOne();
     }
-    public static function ep_saved($ep)
+    public static function ep_saved($ep): bool
     {
         $lastId = $ep->getSubject()->getLastId();
         $table  = $ep->getParam('table');
