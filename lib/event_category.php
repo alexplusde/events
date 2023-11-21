12cfd326387664c86864072c6e3e2ea95f2e6b45
Paul Götz <20090597+schorschy@users.noreply.github.com>
2023-11-21 11:35:04 +0100

    Update event_category.php

--- lib/event_category.php
+++ lib/event_category.php
@@ -1,47 +1,47 @@
 <?php
 class event_category extends \rex_yform_manager_dataset
 {
-    public function getName() :string
+    public function getName(): string
     {
         return $this->name;
     }
-    public function getImage() :string
+    public function getImage(): string
     {
         return $this->image;
     }
-    public function getMedia()
+    public function getMedia(): rex_media
     {
         return rex_media::get($this->image);
     }
     
-    public function getIcon()
+    public function getIcon(): string
     {
         return $this->getValue('icon');
     }
-    public function getPrice()
+    public function getPrice(): string
     {
         return $this->getValue('msg_price');
     }
-    public function getUrl()
+    public function getUrl(): string
     {
         return rex_getUrl('', '', ['event-category-id' => $this->getId()]);
     }
 
-    public function getDateWhere($whereRaw = '')
+    public function getDateWhere($whereRaw = ''): object
     {
         return event_date::query()->joinRelation('event_category_id', 'c')->where('c.id', $this->getId())->whereRaw($whereRaw)->orderBy('startDate`, `startTime', 'DESC')->find();
     }
     
-    public function getRelatedDates($whereRaw = '')
+    public function getRelatedDates($whereRaw = ''): array
     {
         return $this->getDateWhere($whereRaw);
     }
 
-    public function getAttributes()
+    public function getAttributes(): array
     {
         return explode(",", $this->getValue('msg_form_presets'));
     }
-    public function hasAttribute($needle)
+    public function hasAttribute($needle): bool
     {
         return in_array($needle, $this->getAttributes());
     }
