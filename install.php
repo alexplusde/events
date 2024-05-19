2b65b97dcc2b47e7768ac2d900a78b6f839605c6
Alexander Walther <mail@alexplus.de>
2024-05-19 12:41:17 +0200

    Überarbeiteter Install-Prozess
    (Angeglichen an Addon `neues`

--- install.php
+++ install.php
@@ -1,9 +1,42 @@
 <?php
 /* Tablesets aktualisieren */
+if (rex_addon::get('yform') && rex_addon::get('yform')->isAvailable()) {
+    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/install/rex_event.tableset.json'));
+    rex_yform_manager_table::deleteCache();
+}
 
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_event.tableset.json')));
+if (!rex_media::get('events_fallback_image.jpg')) {
+    rex_file::copy(__DIR__ . '/install/events_fallback_image.jpg', rex_path::media('events_fallback_image.jpg'));
+    $data = [];
+    $data['title'] = 'Termine - Fallback-Poster';
+    $data['category_id'] = 0;
+    $data['file'] = [
+        'name' => 'events_fallback_image.jpg',
+        'path' => rex_path::media('events_fallback_image.jpg'),
+    ];
 
-rex_yform_manager_table::deleteCache();
+    rex_media_service::addMedia($data, false);
+}
+
+/* URL-Profile installieren */
+/*
+if (rex_addon::get('url') && rex_addon::get('url')->isAvailable()) {
+    if (false === rex_config::get('events', 'url_profile', false)) {
+        $rex_events_category = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_url_generator_profile WHERE `table_name` = '1_xxx_rex_events_category'"));
+        if (!$rex_events_category) {
+            $query = rex_file::get(__DIR__ . '/install/rex_url_profile_events_category.sql');
+            rex_sql::factory()->setQuery($query);
+        }
+        $rex_events_entry = array_filter(rex_sql::factory()->getArray("SELECT * FROM rex_url_generator_profile WHERE `table_name` = '1_xxx_rex_events_entry'"));
+        if (!$rex_events_entry) {
+            $query = rex_file::get(__DIR__ . 'install/rex_url_profile_events_date.sql');
+            rex_sql::factory()->setQuery($query);
+        }
+        /* URL-Profile wurden bereits einmal installiert, daher nicht nochmals installieren und Entwickler-Einstellungen respektieren */
+/*        rex_config::set('events', 'url_profile', true);
+    }
+}*/
+
 
 $modules = scandir(rex_path::addon("events")."module");
 
