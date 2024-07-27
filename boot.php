e99588138e76d882dc6025dc4e28c317382d9aeb
Alexander Walther <mail@alexplus.de>
2024-07-27 19:16:42 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -296,3 +296,25 @@ rex_extension::register('YFORM_DATA_LIST', function ($ep) {
         );
     }
 });
+
+if (rex::isBackend() && \rex_addon::get('events') && \rex_addon::get('events')->isAvailable() && !rex::isSafeMode()) {
+    $addon = rex_addon::get('events');
+    $pages = $addon->getProperty('pages');
+
+    if(!rex::getConsole()) {
+        $_csrf_key = rex_yform_manager_table::get('rex_event_date')->getCSRFKey();
+        
+        $token = rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+        $params = [];
+        $params['table_name'] = 'rex_event_date'; // Tabellenname anpassen
+        $params['rex_yform_manager_popup'] = '0';
+        $params['_csrf_token'] = $token['_csrf_token'];
+        $params['func'] = 'add';
+
+        $href = rex_url::backendPage('events/date', $params);
+
+        $pages['events']['title'] .= ' <a class="label label-primary tex-primary" style="position: absolute; right: 18px; top: 10px; padding: 0.2em 0.6em 0.3em; border-radius: 3px; color: white; display: inline; width: auto;" href="' . $href . '">+</a>';
+        $addon->setProperty('pages', $pages);
+    }
+}
